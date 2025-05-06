568a5bc6ab86ee1f63180d6ebb32591cb56d3c25
AbhishekMusmiri <abhishekmusmiri07@gmail.com>
2025-05-06 23:36:26 +0530

    Update scipt.js

--- scipt.js
+++ scipt.js
@@ -1,14 +1,6 @@
 function scrollToSection(id) {
-  document.getElementById(id).scrollIntoView({ behavior: "smooth" });
-}
-
-function downloadResume() {
-  const link = document.createElement("a");
-  link.href = "Abhishek Musmiri ATS Resume.pdf";
-  link.download = "Abhishek_Musmiri_Resume.pdf";
-  link.click();
-}
-
-function emailMe() {
-  window.location.href = "mailto:abhishekmusmiri07@gmail.com";
+  const section = document.getElementById(id);
+  if (section) {
+    section.scrollIntoView({ behavior: 'smooth' });
+  }
 }
